d9f5e584ecbdf2713184fd219fe145f50891f84f
Max van Oord <max.van.oord@gmail.com>
2020-02-05 21:40:21 +0100

    added views

--- webmax/polls/urls.py
+++ webmax/polls/urls.py
@@ -5,5 +5,8 @@ from . import views
 # Here we define our url paths which link urls to functions in our view page
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- webmax/polls/views.py
+++ webmax/polls/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+# Parameter 'request' is always needed in a view. other parameters are arguments that are passed by in urls
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def 
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
