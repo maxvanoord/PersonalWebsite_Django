7898e293e36a38501b4531adeec04b7c385351ff
Max van Oord <max.van.oord@gmail.com>
2020-02-07 14:50:27 +0100

    update

--- webmax/polls/urls.py
+++ webmax/polls/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'polls'  # Application namespace for urls -> used to prevent urls overlaps between project apps
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),    # Give your urls a name to call them in href's without hardcoding
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
